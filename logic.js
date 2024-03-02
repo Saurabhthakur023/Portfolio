5ef6defefe8c1aada2ca982cfb3082c974592431
Saurabhthakur023 <saurabhthakur02310@gmail.com>
2024-03-01 20:22:27 -0500

    make some changes

--- logic.js
+++ logic.js
@@ -1,2 +1,24 @@
-let a=1;
-console.log(a);
+let hiremeee=document.querySelector(".hire__me");
+function hire(){
+   console.log(alert("Thank u for hire me"));
+}
+
+let body=document.querySelector("body");
+
+
+let butt=document.querySelector("#but1");
+let currentmode="lightmode";
+
+butt.addEventListener("click",()=>{
+
+   if(currentmode==="lightmode"){
+      currentmode="darkmode";
+     body.classList.add("darkmode");
+   }
+   else{
+      currentmode="lightmode";
+     body.classList.add("lightmode");
+   }
+
+}
+);
