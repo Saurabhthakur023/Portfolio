505268c292d50e867d4db18115923bf9c7004dab
Saurabhthakur023 <saurabhthakur02310@gmail.com>
2024-03-05 14:15:58 -0500

    add dark mode

--- logic.js
+++ logic.js
@@ -3,22 +3,10 @@ function hire(){
    console.log(alert("Thank u for hire me"));
 }
 
-let body=document.querySelector("body");
 
 
-let butt=document.querySelector("#but1");
-let currentmode="lightmode";
+function mode(){
+var body=document.body;
 
-butt.addEventListener("click",()=>{
-
-   if(currentmode==="lightmode"){
-      currentmode="darkmode";
-     body.classList.add("darkmode");
-   }
-   else{
-      currentmode="lightmode";
-     body.classList.add("lightmode");
-   }
-
+  body.classList.toggle("dark-mode");
 }
-);
